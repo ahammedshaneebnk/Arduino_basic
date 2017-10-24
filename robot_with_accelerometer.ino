f7bff5c015967e3a7a90978d8c9f411c6d0d1011
Ahammed Shaneeb N K <shaneeb2015@gmail.com>
2017-10-24 23:36:42 +0530

    Add files via upload

--- robot_with_accelerometer.ino
+++ robot_with_accelerometer.ino
@@ -1,7 +1,6 @@
 int xPin = A1;
 int yPin = A0;
 int xRest, yRest, xRead, yRead;
-double Gx, Gy;
 int m1a = 9, m1b = 8, m2a = 4, m2b = 3;
 
 void setup() {
@@ -22,31 +21,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int xRead = analogRead(xPin) - xRest;
-  int yRead = analogRead(yPin) - yRest;
-  Gx = xRead / 67.584;
-  Gy = yRead / 67.584;
-  Serial.print(Gx);
+  int xRead = analogRead(xPin);
+  int yRead = analogRead(yPin);
+  Serial.print(xRead);
   Serial.print("\t");
-  Serial.println(Gy);
+  Serial.println(yRead);
   
-                    if (Gy > -0.2)
-                      {
-                        //turn to right
-                        digitalWrite(m1a, LOW);
-                        digitalWrite(m1b, HIGH);
-                        digitalWrite(m2a, LOW);
-                        digitalWrite(m2b, HIGH);
-                        }
-                    else if (Gy < -01.4)
-                      {
-                        //turn to left
-                        digitalWrite(m1a, HIGH);
-                        digitalWrite(m1b, LOW);
-                        digitalWrite(m2a, HIGH);
-                        digitalWrite(m2b, LOW);
-                        }
-                    else if (Gx < -0.25)
+                    if ((xRead < 270) && (yRead > 300))
                       {
                         //go straight
                         digitalWrite(m1a, HIGH);
@@ -54,11 +35,27 @@ void loop() {
                         digitalWrite(m2a, LOW);
                         digitalWrite(m2b, HIGH);
                         }
-                    else if (Gx > 1.1)
+                    else if ((xRead > 350) && (yRead > 300))
                       {
                         //go back
                         digitalWrite(m1a, LOW);
                         digitalWrite(m1b, HIGH);
+                        digitalWrite(m2a, HIGH);
+                        digitalWrite(m2b, LOW);
+                        }
+                    else if ((xRead > 300) && (yRead > 380))
+                      {
+                        //turn to right
+                        digitalWrite(m1a, LOW);
+                        digitalWrite(m1b, HIGH);
+                        digitalWrite(m2a, LOW);
+                        digitalWrite(m2b, HIGH);
+                        }
+                    else if ((xRead > 290) && (yRead < 280))
+                      {
+                        //turn to left
+                        digitalWrite(m1a, HIGH);
+                        digitalWrite(m1b, LOW);
                         digitalWrite(m2a, HIGH);
                         digitalWrite(m2b, LOW);
                         }
